b854d1affb04175e35903aa4eb7d49b69020ebcc
Parwinder Bhagat <singhparwinder@me.com>
2020-07-12 18:59:06 -0500

    moving helloWorld logic to controller

--- controller.ts
+++ controller.ts
@@ -1,3 +1,9 @@
+export const helloWorld = ({ response }: { response: any }) => {
+  response.body = {
+    message: "Hello World!",
+  };
+};
+
 export const calculateFizzBuzz = async ({ params, response }: { params: { id: string }, response: any }) => {
     response.status = 200;
     response.body = {
--- routes.ts
+++ routes.ts
@@ -1,14 +1,10 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-// import { calculateFizzBuzz } from "./controller.ts";
+import { helloWorld, calculateFizzBuzz } from "./controller.ts";
 
 const router = new Router();
 
-router.get("/", ({ response }: { response: any }) => {
-  response.body = {
-    message: "Hello World!",
-  };
-});
-
-// router.get("/api/v1/fizzbuzz/:id", calculateFizzBuzz);
+router
+    .get("/", helloWorld)
+    .get("/fizzbuzz", helloWorld);
 
 export default router;
