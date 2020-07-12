dac497b43bb87ba6da8e574ece71169134000aea
Parwinder Bhagat <singhparwinder@me.com>
2020-07-12 18:55:26 -0500

    moving out router from server

--- routes.ts
+++ routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { calculateFizzBuzz } from "./controller.ts";
+// import { calculateFizzBuzz } from "./controller.ts";
 
 const router = new Router();
 
-router.get("/api/v1/fizzbuzz/:id", calculateFizzBuzz);
+router.get("/", ({ response }: { response: any }) => {
+  response.body = {
+    message: "Hello World!",
+  };
+});
+
+// router.get("/api/v1/fizzbuzz/:id", calculateFizzBuzz);
 
 export default router;
--- server.ts
+++ server.ts
@@ -1,22 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { bold, green } from "https://deno.land/std@0.53.0/fmt/colors.ts";
+import router from "./routes.ts";
 const port = 8000;
 const app = new Application();
-const router = new Router();
-
-router.get("/", ({ response }: { response: any }) => {
-  response.body = {
-    message: "Hello World!",
-  };
-});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.addEventListener("listen", ({ secure, hostname, port }) => {
-    const protocol = secure ? "https://" : "http://";
-    const url = `${protocol}${hostname ?? "localhost"}:${port}`;
-    console.log(`${bold("server is listening on :")} ${green(url)}`);
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  console.log(`${bold("server is listening on :")} ${green(url)}`);
 });
 
 await app.listen({ port });
